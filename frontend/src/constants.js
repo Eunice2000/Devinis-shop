28e377bfcc920f5ba9640abc601c68601df8ff93
Eunice2000 <adediraneunice20@gmail.com>
2023-05-18 14:25:09 +0000

    deploy client

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,6 @@
 // export const BASE_URL =
 //   process.env.NODE_ENV === 'develeopment' ? 'http://localhost:5000' : '';
-export const BASE_URL = 'http://52.87.163.132/'; // If using proxy
+export const BASE_URL = 'http://54.164.190.138/'; // If using proxy
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
 export const ORDERS_URL = '/api/orders';
